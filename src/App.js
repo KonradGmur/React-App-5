2f59972d6f217ed84d8d482df7460c37b2146167
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-11 14:06:12 +0200

    App.js- adding error messages

--- src/App.js
+++ src/App.js
@@ -8,11 +8,24 @@ class App extends Component {
     username: '',
     email: '',
     pass: '',
-    accept: false
+    accept: false,
+
+    errors: {
+      username: false,
+      email: false,
+      pass: false,
+      accept: false,
+    }
+  }
+
+  messages = {
+    username_incorrect: "Nazwa musi być dłuższa niż 10 znaków i nie może zajmować spacji",
+    email_incorrect: " Brak @ w emailu",
+    password_incorrect: "Hasło musi mieć 8 znaków",
+    accept_incorrect: "Niepotwierdzona zgoda"
   }
 
   handleChange = (e) => {
-
     const name = e.target.name;
     const type = e.target.type;
 
@@ -41,18 +54,23 @@ class App extends Component {
         <form onSubmit={this.handleSubmit} noValidate>
           <label htmlFor="user">Twoje imię:
             <input type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange} />
+            {this.state.errors.username && <span>{this.messages.username_incorrect}</span>}
           </label>
 
           <label htmlFor="email">Twoj e-mail:
             <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleChange} />
+            {this.state.errors.email && <span>{this.messages.email_incorrect}</span>}
           </label>
 
           <label htmlFor="password">Twoje hasło:
             <input type="password" id="password" name="pass" value={this.state.pass} onChange={this.handleChange} />
+            {this.state.errors.pass && <span>{this.messages.password_incorrect}</span>}
           </label>
 
-          <label htmlFor="accept"></label>
-          <input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleChange} /> Wyrażam zgodę na wszystko
+          <label htmlFor="accept">
+            <input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleChange} /> Wyrażam zgodę na wszystko
+          </label>
+          {this.state.errors.accept && <span>{this.messages.accept_incorrect}</span>}
           <button>Zapisz się</button>
         </form>
       </div>
