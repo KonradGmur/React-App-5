aac831c1743d8a9741e98f28dd6d87b179792582
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-11 13:42:42 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class App extends Component {
   state = {
+    username: '',
+    email: '',
+    pass: '',
+    accept: false
+  }
+
+  handleChange = (e) => {
+
+    const name = e.target.name;
+    const type = e.target.type;
+
+    if (type === "text" || type === "passwod" || type === "email") {
+      const value = e.target.value;
+      this.setState({
+        [name]: value
+      })
+    } else if (type === "checkbox") {
+      const checked = e.target.checked;
+      this.setState({
+        [name]: checked
+      })
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Działa');
 
   }
+
   render() {
-    return ( 
-      
-     );
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit} noValidate>
+          <label htmlFor="user">Twoje imię:
+            <input type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="email">Twoj e-mail:
+            <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="password">Twoje hasło:
+            <input type="password" id="password" name="pass" value={this.state.pass} onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="accept"></label>
+          <input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleChange} /> Wyrażam zgodę na wszystko
+          <button>Zapisz się</button>
+        </form>
+      </div>
+    )
   }
 }
 
